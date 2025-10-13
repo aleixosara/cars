edffdda850dd8a621c687de696798a061afa912e
sara <20233005742@estudantes.ifpr.edu.br>
2025-10-13 08:18:47 -0300

    Update

--- src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -1,15 +1,25 @@
 package br.edu.ifpr.cars.api;
 
-import ord.spirngframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.spirngframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequesMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifpr.cars.domain.DriverRepository;
 
 @Service
 @RestController
+@RequestMapping (produces = MediaType.APPLICATION_JSON_VALUE)
 public class DriverController {
     
     @Autowired // injeção de dependência
     DriverRepository driverRepository; // Objeto instancia do repositório
+
+    @GetMapping("/drivers")
+    public List<Driver> listDrivers() {
+        return driverRepository.findAll();
+    }
 }
