b750ac4f14837caa5e270bccdd6828253a6ae6d6
sara aleixo <20233005742@estudantes.ifpr.edu.br>
2025-10-20 08:15:07 -0300

    Update

--- src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -2,24 +2,54 @@ package br.edu.ifpr.cars.api;
 
 import java.util.List;
 
-import org.spirngframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequesMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import br.edu.ifpr.cars.domain.Driver;
 import br.edu.ifpr.cars.domain.DriverRepository;
 
 @Service
 @RestController
-@RequestMapping (produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class DriverController {
-    
+
     @Autowired // injeção de dependência
-    DriverRepository driverRepository; // Objeto instancia do repositório
+    DriverRepository driverRepository; // objeto instancia do repositório
 
     @GetMapping("/drivers")
     public List<Driver> listDrivers() {
         return driverRepository.findAll();
+    }
+
+    // busca por id
+    // definir uma Exception personalizada
+    @GetMapping("/drivers/{id}")
+    public Driver findDriver(@PathVariable("id") Long id) {
+        return driverRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/driver")
+    public Driver createDriver(@RequestBody Driver driver) {
+        return driverRepository.save(driver);
+    }
+
+    // update
+    @PutMapping("/drivers/{id}")
+    public Driver fullUpdateDriver(@PathVariable("id") Long id, @RequestBody Driver driver) {
+        Driver foundDriver = findDriver(id);
+        foundDriver.setName(driver.getName());
+        foundDriver.setBirthDate(driver.getBirthDate());
+        return driverRepository.save(foundDriver);
     }
 }
